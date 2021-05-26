db5a5037201e2fc12cc38c764c01fbcf6be3b07c
neksys-on <neksyz@gmail.com>
2021-05-26 18:36:21 +0300

    Update yandexMetrica.js

--- components/yandexMetrica.js
+++ components/yandexMetrica.js
@@ -1,4 +1,5 @@
 
+// 78632355 my YM
 
 export default function YandexMetrica() {
   return (
@@ -8,13 +9,13 @@ export default function YandexMetrica() {
          m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-         ym(78632355, "init", {
+         ym(79629826, "init", {
               clickmap:true,
               trackLinks:true,
               accurateTrackBounce:true
          });`}
       </script>
-      <div><img crossOrigin="anonymous" src="https://mc.yandex.ru/watch/78632355" style={{position: 'absolute', left:'-9999px'}} alt=""></img></div>
+      <div><img crossOrigin="anonymous" src="https://mc.yandex.ru/watch/79629826" style={{position: 'absolute', left:'-9999px'}} alt=""></img></div>
     </>
   )
 }
